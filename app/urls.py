c2b8193193e5aa8b862ba407f7110df1bc082ef2
laurettemongina <monginalaurette2gmail.com>
2021-10-12 07:09:02 +0300

    Adds url for search,location and homepage.

---
+++ app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    path('location/<int:location_id>/', views.location, name='location'),
+    # path('image/<int:image_id>/', views.single_photo, name='single_photo'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
